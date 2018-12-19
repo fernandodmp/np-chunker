a29e22771fb8c7addfd8c272a70ce2903e3518c3
Fernando Passos <30850453+fernandodmp@users.noreply.github.com>
2018-12-19 12:08:38 -0300

    Adjusts tree printing

--- classificator.py
+++ classificator.py
@@ -1,62 +1,69 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
-from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.svm import SVC, SVR
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.preprocessing import LabelEncoder
+import pydotplus
 
-df = pd.read_csv("sintagmas.csv")
+def classify():
+    df = pd.read_csv("sintagmas.csv")
 
-mapping = {
-    "BNP": 0,
-    "BVP": 1,
-    "INP": 2,
-    "IVP": 3,
-    "ENP": 4,
-    "EVP": 5,
-    "NONE": 6,
-}
+    mapping = {
+        "BNP": 0,
+        "BVP": 1,
+        "INP": 2,
+        "IVP": 3,
+        "ENP": 4,
+        "EVP": 5,
+        "NONE": 6,
+    }
 
-df = df.drop(df.columns[0], axis=1)
-df["Anterior-Tag"] = df["Anterior-Tag"].apply(lambda x: mapping[x])
-df["POS"] = pd.Categorical(df["POS"]).codes
-df["Tag"] = df["Tag"].apply(lambda x: mapping[x])
+    labelEncoder = LabelEncoder()
+    df = df.drop(df.columns[0], axis=1)
+    y = labelEncoder.fit_transform(df["Tag"])
+    x =  pd.get_dummies(df.drop(["Tag"], axis=1), drop_first=True)
+    #df["Anterior-Tag"] = df["Anterior-Tag"].apply(lambda x: mapping[x])
+    #df["POS"] = pd.Categorical(df["POS"]).codes
+    #df["Tag"] = df["Tag"].apply(lambda x: mapping[x])
 
-# clf = Pipeline([
-#     # 0.635
-#     ('clf', GaussianNB())
-# ])
-#
-# clf = Pipeline([
-#     # 0.739
-#     ('clf', DecisionTreeClassifier())
-# ])
-#
-clf = Pipeline([
-    # 0.731
-    ('clf', KNeighborsClassifier())
-])
-# clf = Pipeline([
-#     # 0.733
-#     ('clf', SVC())
-# ])
-clf = Pipeline([
-    # 0.745
-    ('clf', RandomForestClassifier())
-])
+    # clf = Pipeline([
+    #     # 0.635
+    #     ('clf', GaussianNB())
+    # ])
+    #
+    
+    # 0.763671875
+    clf = DecisionTreeClassifier()
+        
+    #
+    #clf = Pipeline([
+    #     # 0.731
+    #   ('clf', KNeighborsClassifier())
+    #])
+    # clf = Pipeline([
+    #     # 0.733
+    #     ('clf', SVC())
+    # ])
+    #clf = Pipeline([
+        # 0.745
+    #    ('clf', RandomForestClassifier())
+    #])
 
-grid = GridSearchCV(clf, {}, n_jobs=-1, cv=100, verbose=True)
+    grid = GridSearchCV(clf, {}, n_jobs=-1, cv=100, verbose=True)
+    grid.fit(x, y)
+    print("Params")
+    print(grid.best_params_)
+    print("Score")
+    print(grid.best_score_)
+    model = grid.best_estimator_
+    print(model)
+    export_graphviz(model, feature_names=x.columns.values,  out_file='Tree.dot', filled=True, rounded=True, class_names=labelEncoder.classes_)
 
-y = df["Tag"]
-x = df.drop(["Tag"], axis=1)
 
-grid.fit(x, y)
-print("Params")
-print(grid.best_params_)
-print("score")
-print(grid.best_score_)
+if __name__ == '__main__':
+    classify()
 
 # print(df.groupby("Anterior-Tag").count())
 
