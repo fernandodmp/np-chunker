395ed9a53f637c675ae4cd68b491609784e99193
Fernando Passos <30850453+fernandodmp@users.noreply.github.com>
2018-12-20 10:36:01 -0300

    Adds more examples

--- classificator.py
+++ classificator.py
@@ -1,64 +1,34 @@
 import pandas as pd
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report
 import pydotplus
 
 def classify():
     df = pd.read_csv("sintagmas.csv")
-
-    mapping = {
-        "BNP": 0,
-        "BVP": 1,
-        "INP": 2,
-        "IVP": 3,
-        "ENP": 4,
-        "EVP": 5,
-        "NONE": 6,
-    }
-
     labelEncoder = LabelEncoder()
     df = df.drop(df.columns[0], axis=1)
+
     y = labelEncoder.fit_transform(df["Tag"])
     x =  pd.get_dummies(df.drop(["Tag"], axis=1), drop_first=True)
-    #df["Anterior-Tag"] = df["Anterior-Tag"].apply(lambda x: mapping[x])
-    #df["POS"] = pd.Categorical(df["POS"]).codes
-    #df["Tag"] = df["Tag"].apply(lambda x: mapping[x])
-
-    # clf = Pipeline([
-    #     # 0.635
-    #     ('clf', GaussianNB())
-    # ])
-    #
     
-    # 0.763671875
-    clf = DecisionTreeClassifier()
-        
-    #
-    #clf = Pipeline([
-    #     # 0.731
-    #   ('clf', KNeighborsClassifier())
-    #])
-    # clf = Pipeline([
-    #     # 0.733
-    #     ('clf', SVC())
-    # ])
-    #clf = Pipeline([
-        # 0.745
-    #    ('clf', RandomForestClassifier())
-    #])
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
 
-    grid = GridSearchCV(clf, {}, n_jobs=-1, cv=100, verbose=True)
-    grid.fit(x, y)
+    clf = DecisionTreeClassifier() # 0.763671875
+    grid = GridSearchCV(clf, {'criterion': ['gini', 'entropy']}, n_jobs=-1, cv=100, verbose=True)
+    grid.fit(X_train, y_train)
+    y_pred = grid.predict(X_test)
     print("Params")
     print(grid.best_params_)
     print("Score")
     print(grid.best_score_)
     model = grid.best_estimator_
-    print(model)
+
+    print(classification_report(labelEncoder.inverse_transform(y_test), labelEncoder.inverse_transform(y_pred)))
     export_graphviz(model, feature_names=x.columns.values,  out_file='Tree.dot', filled=True, rounded=True, class_names=labelEncoder.classes_)
 
 
